db7b789e68e7ca6cb6f790e98aa8f04bfc8cfee5
zhangzongkai <zhang.zongkai@foxmail.com>
2023-07-03 22:01:31 +0800

    feat: add user option

--- options.lua
+++ options.lua
@@ -1,22 +1,40 @@
 -- set vim options here (vim.<first_key>.<second_key> = value)
 return {
   opt = {
-    -- set to true or false etc.
-    relativenumber = true, -- sets vim.opt.relativenumber
-    number = true, -- sets vim.opt.number
-    spell = false, -- sets vim.opt.spell
-    signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-    wrap = false, -- sets vim.opt.wrap
+    -- Line setting
+    relativenumber = true,
+    number = true,
+    wrap = false,
+    cursorline = true,
+    colorcolumn = "120",
+
+    -- Tabs & Indentation
+    tabstop = 4,
+    shiftwidth = 4,
+    expandtab = true,
+    autoindent = true,
+
+    -- Search settings
+    ignorecase = true,
+    smartcase = true,
+
+    -- Clipboard
+    clipboard = "unnamedplus",
+
+    -- File
+    backup = false,
+    writebackup = false,
+    swapfile = false,
   },
   g = {
-    mapleader = " ", -- sets vim.g.mapleader
-    autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-    cmp_enabled = true, -- enable completion at start
-    autopairs_enabled = true, -- enable autopairs at start
-    diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-    icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
-    ui_notifications_enabled = true, -- disable notifications when toggling UI elements
-    resession_enabled = false, -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
+    mapleader = " ",
+    autoformat_enabled = true,
+    cmp_enabled = true,
+    autopairs_enabled = true,
+    diagnostics_mode = 3,
+    icons_enabled = true,
+    ui_notifications_enabled = true,
+    resession_enabled = false,
   },
 }
 -- If you need more control, you can use the function()...end notation
