d3cbe98521b46b8e62e8e15d771fb24e561603b1
zhangzongkai <zhangzongkai@lixiang.com>
2023-07-04 16:42:08 +0800

    feat: add treesitter default install language

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,7 +3,20 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "bash",
+      "c",
+      "cmake",
+      "cpp",
+      "dockerfile",
+      "html",
+      "java",
+      "javascript",
+      "json",
+      "lua",
+      "python",
+      "sql",
+      "vim",
+      "yaml",
     })
   end,
 }
